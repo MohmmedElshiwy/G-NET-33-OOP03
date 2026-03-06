fddae5366a0675efdb9fd062034345d939608af6
Toji <Toji@DESKTOP-5F6RM58>
2026-03-06 11:26:48 -0800

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Part 01 : Theoretical Questions
+            /*
+             Q1 : Identify the type of relationship in each scenario below (Inheritance, Association, Aggregation, Composition, or Dependency):
+
+
+             */
+
+
+            #endregion
+
+
+
+        }
+    }
+}
